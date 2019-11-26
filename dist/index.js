9d7f8f8146a9a9299dc2511af6587343f6fac776
Karl Hulme <karlhulme@hotmail.com>
2019-11-26 10:58:27 +0000

    Possible fix for package.json name

--- dist/index.js
+++ dist/index.js
@@ -4801,7 +4801,7 @@ function escape(s) {
 const run = async ({ branchName, releaseVersion, releaseNotes, owner, repo, getContents, createOrUpdateFile }) => {
   console.log('made it into the action')
 
-  const contentsResult = await getContents({ owner, repo, path: './package.json', ref: branchName })
+  const contentsResult = await getContents({ owner, repo, path: 'package.json', ref: branchName })
 
   console.log(JSON.stringify(contentsResult.data))
 
