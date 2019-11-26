c27b789c2eee64719f8b3ff19aef857d3a270839
Karl Hulme <karlhulme@hotmail.com>
2019-11-26 15:50:04 +0000

    Log the response from fetch package json

--- dist/index.js
+++ dist/index.js
@@ -4688,7 +4688,7 @@ function Octokit(plugins, options) {
 
 const fetchPackageJson = async (owner, repo, branchName, getContents) => {
   const contentsResult = await getContents({ owner, repo, path: 'package.json', ref: branchName })
-
+  console.log(JSON.stringify(contentsResult.data, null, 2))
   return {
     sha: contentsResult.data.sha,
     content: JSON.parse(Buffer.from(contentsResult.data.content, 'base64').toString('utf8'))
--- src/fetchPackageJson.js
+++ src/fetchPackageJson.js
@@ -1,6 +1,6 @@
 const fetchPackageJson = async (owner, repo, branchName, getContents) => {
   const contentsResult = await getContents({ owner, repo, path: 'package.json', ref: branchName })
-
+  console.log(JSON.stringify(contentsResult.data, null, 2))
   return {
     sha: contentsResult.data.sha,
     content: JSON.parse(Buffer.from(contentsResult.data.content, 'base64').toString('utf8'))
