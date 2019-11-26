aa04645815b1072f1b7e27ec53a93aedf512b8ee
Karl Hulme <karlhulme@hotmail.com>
2019-11-26 10:50:11 +0000

    Try to use getContents

--- src/run.js
+++ src/run.js
@@ -4,6 +4,12 @@
  */
 const run = async ({ branchName, releaseVersion, releaseNotes, owner, repo, getContents, createOrUpdateFile }) => {
   console.log('made it into the action')
+
+  const contentsResult = await getContents({ owner, repo, path: './package.json', ref: branchName })
+
+  console.log(JSON.stringify(contentsResult.data))
+
+  return {}
 }
 
 module.exports = run
